be090fe509b37080be6b8c6fc6e46c1e8f5e5d18
hansrajathole <hansrajathole1@gmail.com>
2026-09-11 22:37:48 +0530

    leetcode questions

---
+++ Lec3/ArrayDemo.java
@@ -0,0 +1,29 @@
+package Lec3;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        
+        // int arr[] = new int[5];
+    
+    
+        // for(int i = 0 ; i<arr.length ; i++){
+        //     arr[i] = i;
+        // }
+
+
+        // for(int i = 0 ; i<arr.length ; i++){
+        //     System.out.println(i+" ---> "+arr[i]);
+           
+        // }
+
+
+        int arr[] = {20,30,40,50};
+         for(int i = 0 ; i<arr.length ; i++){
+            System.out.println(i+" ---> "+arr[i]);
+           
+        }
+
+
+
+    }
+}
